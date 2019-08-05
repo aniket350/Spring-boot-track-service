c2ac38bb2e13aac2321fe54cb3f73c04a3640a6d
aniket350 <subarni100@gmail.com>
2019-08-05 12:30:12 +0530

    Implemented swagger

--- src/main/java/com/stackroute/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
     @Bean
     public Docket productApi() {
+        //Implementing Swagger2
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.stackroute.controller"))
